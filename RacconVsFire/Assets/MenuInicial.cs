1f0ca1883bf988ee2dfe6eddc010b5299a8739ef
Mariana Nazaret Solis Torres <z.areth94@hotmail.com>
2022-03-06 04:40:57 -0600

    cambio de pantalla

--- RacconVsFire/Assets/MenuInicial.cs
+++ RacconVsFire/Assets/MenuInicial.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuInicial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  
+    public void CargaNivel1()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+     SceneManager.LoadScene("Nivel 1");
+        Debug.Log("Carga Nivel 1");
+    } 
 }
